6e3723235c13c41b7cd198042299dc2faea3f3de
Dawid Brzeski <daw.brzeski@gmail.com>
2021-09-02 10:27:29 +0200

    Add some text to result function

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -2,7 +2,12 @@ import { Wrapper } from "./styled";
 
 export const Result = ({ result }) => (
     <Wrapper>
-        Otrzymasz:&nbsp;  
+        Za&nbsp;
+        {result !== undefined && (
+            <strong>
+                {result.sourceAmount}&nbsp;PLN&nbsp;
+            </strong>)}
+        otrzymasz&nbsp;
         {result !== undefined && (
             <strong>
                 {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
