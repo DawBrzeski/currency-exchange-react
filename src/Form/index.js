9f70cbf60c8a7325ff7d286617133125adb8b5e2
Dawid Brzeski <daw.brzeski@gmail.com>
2021-09-02 09:46:03 +0200

    changed base currency

--- src/Form/index.js
+++ src/Form/index.js
@@ -26,59 +26,58 @@ export const Form = ({ calculateResult, result }) => {
                 : (
                     ratesData.state === "error" ? (
                         <Failure>
-                            Coś poszło nie tak. Sprawdz czy amsz polączenie z internetem...
+                            Coś poszło nie tak. Sprawdz czy masz polączenie z internetem...
                         </Failure>
                     ) : (
                         <div><Clock></Clock>
-                        <Label>
-                            <LabelText>
-                                Wpisz kwotę w EUR:
-                            </LabelText>
-                            <br></br>
-                            <select >
-                                <option defaultValue="eur" selected>EUR</option>
-                            </select>
-                        </Label>
-                        <Label >
-                            <input
-                                type="number"
-                                value={amount}
-                                onChange={({ target }) => setAmount(target.value)}
-                                step="0.01"
-                                min="1"
-                                max="1000000000000"
-                                placeholder="Mam"
-                                required
-                            />
-                        </Label>
-                        <Label> <LabelText>Wybierz walute, na
-                            która chcesz
-                            zamienić:</LabelText><br></br>
-                            <select 
-                            defaultValue="EUR"
-                                   onChange={({ target }) => setCurrency(target.value)}
-                            >
-                                {Object.keys(ratesData.rates).map(((currency) => (
-                                    <option
-                                        key={currency}
-                                        value={currency}
-                                    >
-                                        {currency}
-                                    </option>
-                                )))}
-                            </select>
-                        </Label>
-                        <input type="number" placeholder="Otrzymam"
-                            value="" disabled />
-            
-                        <Buttons>
-                            <Button>Oblicz</Button>
-                            <Button type="reset">Zresetuj </Button>
-                        </Buttons>
-                        <Result result={result} /></div>
+                            <Label>
+                                <LabelText>
+                                    Wpisz kwotę w PLN:
+                                </LabelText>
+                                <br></br>
+                                <select >
+                                    <option defaultValue="pln" selected>PLN</option>
+                                </select>
+                            </Label>
+                            <Label >
+                                <input
+                                    type="number"
+                                    value={amount}
+                                    onChange={({ target }) => setAmount(target.value)}
+                                    step="0.01"
+                                    min="1"
+                                    max="1000000000000"
+                                    placeholder="Mam"
+                                    required
+                                />
+                            </Label>
+                            <Label> <LabelText>Wybierz walute, na
+                                która chcesz
+                                zamienić:</LabelText><br></br>
+                                <select
+                                    defaultValue="EUR"
+                                    onChange={({ target }) => setCurrency(target.value)}
+                                >
+                                    {Object.keys(ratesData.rates).map(((currency) => (
+                                        <option
+                                            key={currency}
+                                            value={currency}
+                                        >
+                                            {currency}
+                                        </option>
+                                    )))}
+                                </select>
+                            </Label>
+                            <input type="number" placeholder="Otrzymam"
+                                value="" disabled />
+
+                            <Buttons>
+                                <Button>Oblicz</Button>
+                            </Buttons>
+                            <Result result={result} /></div>
                     ))}
 
-            
+
         </Wrapper>
 
 
